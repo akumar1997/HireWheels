b9ea53744f4f1d8ecb37ba9baade8a238e372258
Arnav <ak.arnavkumar1997@gmail.com>
2021-01-25 02:56:12 +0530

    TODO 3.2: Providing Custom Query Methods for the UserDao Interface

--- src/main/java/com/upgrad/hirewheels/dao/UsersDAO.java
+++ src/main/java/com/upgrad/hirewheels/dao/UsersDAO.java
@@ -4,4 +4,24 @@ import com.upgrad.hirewheels.entities.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsersDAO extends JpaRepository<Users,Integer> {
+    //TODO 3.2: Providing Custom Query Methods for the UserDao Interface
+    //
+
+    //Write custom query methods inside the UserDao interface to find a user by his:
+
+    //First name
+
+    Users findByFirstName(String firstName);
+
+    //First name or last name
+
+    Users findByFirstNameOrLastName(String firstName,String lastName);
+
+    //Email
+
+    Users findByEmail(String email);
+
+    //Mobile Number
+
+    Users findByMobileNo(String mobileNo);
 }
