83bc94c1e8fb0bcaabe94c23d33956e2db3bb11a
Arnav <ak.arnavkumar1997@gmail.com>
2021-02-10 18:35:43 +0530

    Checkpoint 9 done: Logging the messages of endpoints implemented in the AdminController class

--- src/main/java/com/upgrad/hirewheels/controllers/AdminController.java
+++ src/main/java/com/upgrad/hirewheels/controllers/AdminController.java
@@ -5,6 +5,8 @@ import com.upgrad.hirewheels.entities.Vehicle;
 import com.upgrad.hirewheels.exceptions.VehicleDetailsNotFoundException;
 import com.upgrad.hirewheels.services.VehicleServiceImpl;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -21,12 +23,15 @@ public class AdminController {
     @Autowired
     ModelMapper modelmapper;
 
+    private static final Logger logger = LoggerFactory.getLogger(AdminController.class);
+
 
     @PostMapping(value="/vehicles",consumes= MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity newBooking(@RequestBody VehicleDto vehicleDto) {
+    public ResponseEntity newVehicle(@RequestBody VehicleDto vehicleDto) {
         Vehicle newVehicle = modelmapper.map(vehicleDto, Vehicle.class);
         Vehicle savedVehicle = vehicleService.addVehicle(newVehicle);
         VehicleDto savedVehicleDto = modelmapper.map(savedVehicle, VehicleDto.class);
+        logger.debug("Accepted new vehicle details",savedVehicle);
         return new ResponseEntity<>(savedVehicleDto, HttpStatus.CREATED);
     }
     @PutMapping(value ="/vehicles/{id}",consumes =MediaType.APPLICATION_JSON_VALUE )
@@ -35,6 +40,7 @@ public class AdminController {
         Vehicle vehicle= vehicleService.getVehicleDetails(id);
         vehicle.setAvailabilityStatus(availabilityStatus);
         vehicleService.addVehicle(vehicle);
+        logger.debug("Update vehicle availability details : vehicle id :" + id, vehicle);
         return new ResponseEntity<>("saved",HttpStatus.OK);
     }
 }
